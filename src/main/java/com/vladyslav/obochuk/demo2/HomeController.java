b5f941791436970a4576a16d44dbc52e8ec447ef
Vladyslav Obochuk <vladyslav_obochuk@epam.com>
2021-07-18 12:42:51 +0300

    add application

---
+++ src/main/java/com/vladyslav/obochuk/demo2/HomeController.java
@@ -0,0 +1,12 @@
+package com.vladyslav.obochuk.demo2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String getSomething() {
+        return "Hello world!";
+    }
+}
